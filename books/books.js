21254fb3b1766eaaee4dd79fcfa1ea578e2920ee
Gangula2 <gangulaanwesh@gmail.com>
2022-11-09 00:11:20 +0530

    connected to mongodb + +installed nodemon

--- books/books.js
+++ books/books.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/microservice_test", () => {
+    console.log("Database is connected");
+});
+
 app.listen(4545, () => {
     console.log("Server running at 4545");
 });
