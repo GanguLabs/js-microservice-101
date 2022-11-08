878070a91d200c0b927a8bef880d8687e2322b21
Gangula2 <gangulaanwesh@gmail.com>
2022-11-09 00:49:54 +0530

    get all books or book by id

--- books/books.js
+++ books/books.js
@@ -1,3 +1,5 @@
+require("./models/Book")
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -17,7 +19,52 @@ app.get("/", (req, res) => {
     res.send("This is our main endpoint");
 })
 
+const Book = mongoose.model("Book");
 app.post("/book", (req, res) => {
-    console.log(req.body)
+    var newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        numberPages: req.body.numberPages,
+        publisher: req.body.publisher
+    }
+
+    var book = new Book(newBook);
+
+    book.save().then(() => {
+        console.log("new book created");
+    }).catch((err) => {
+        if (err) {
+            throw err;
+        }
+    })
+
+    res.send("A new book created successfully")
+
     res.send("Testing our book route!");
 });
+
+app.get("/books", (req, res) => {
+    Book.find()
+        .then((books) => {
+            res.json(books);
+        })
+        .catch((err) => {
+            throw err
+        })
+})
+
+app.get("/book/:id", (req, res) => {
+    Book.findById(req.params.id)
+        .then((book) => {
+
+            if (book) {
+                res.json(book);
+
+            } else {
+                res.sendStatus(404);
+            }
+
+        }).catch((err) => {
+            throw err;
+        });
+})
