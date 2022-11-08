4ac3c103725d26156f65dd070770d07f1e34f7ea
Gangula2 <gangulaanwesh@gmail.com>
2022-11-09 02:54:39 +0530

    working on "Orders" post api - error

--- orders/orders.js
+++ orders/orders.js
@@ -1,6 +1,39 @@
+require("./models/Order")
+
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+app.use(bodyParser.json());
+mongoose.connect("mongodb://OrdersOwner:1234@127.0.0.1:27017/microservice_test", () => {
+    console.log("Orders Database is connected");
+});
+
+const Order = mongoose.model("Order");
+
+app.post("/order", (req, res) => {
+    console.log(mongoose.Types.ObjectId(req.body.CustomerID))
+
+    var newOrder = {
+        CustomerID: mongoose.Types.ObjectId(req.body.CustomerID),
+        BookID: mongoose.Types.ObjectId(req.body.BookID),
+        initialDate: req.body.initialDate,
+        deliveryDate: req.body.deliveryDate
+    }
+    var order = new Order(newOrder);
+
+    order.save().then(() => {
+        console.log("Order created successfullt");
+    }).catch((err) => {
+        if (err) {
+            throw err
+        }
+    })
+
+    res.send("new order created successfully");
+
+})
 app.listen(7777, () => {
     console.log("Orders Server running at 7777")
 })
