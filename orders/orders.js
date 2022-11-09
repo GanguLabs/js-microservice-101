d2cc8b99314fe52b690e7a1632ee06964772a662
Anwesh Gangula <gangulaanwesh@gmail.com>
2022-11-09 12:39:34 +0530

    orders get request

--- orders/orders.js
+++ orders/orders.js
@@ -13,8 +13,6 @@ mongoose.connect("mongodb+srv://rootAdmin:1234@main-cluster.nlwzwip.mongodb.net/
 const Order = mongoose.model("Order");
 
 app.post("/order", (req, res) => {
-    console.log(mongoose.Types.ObjectId(req.body.CustomerID))
-
     var newOrder = {
         CustomerID: mongoose.Types.ObjectId(req.body.CustomerID),
         BookID: mongoose.Types.ObjectId(req.body.BookID),
@@ -24,16 +22,40 @@ app.post("/order", (req, res) => {
     var order = new Order(newOrder);
 
     order.save().then(() => {
-        console.log("Order created successfullt");
+        res.send("new order created successfully");
+        ;
     }).catch((err) => {
         if (err) {
             throw err
         }
     })
-
-    res.send("new order created successfully");
-
 })
+
+
+app.get("/orders", (req, res) => {
+    Order.find()
+        .then((books) => {
+            res.json(books)
+        })
+        .catch((err) => {
+            if (err) {
+                throw err
+            }
+        })
+})
+
+app.get("/order/:id", (req, res) => {
+    Order.findById(req.params.id)
+        .then((order) => {
+            res.json(order);
+        })
+        .catch((err) => {
+            if (err) {
+                throw err
+            }
+        })
+})
+
 app.listen(7777, () => {
     console.log("Orders Server running at 7777")
 })
